19c62f6eceece70ac9dfd5ababe0ec80085a7e14
ENDE-ABAY <abayfeleke13@gmail.com>
2024-06-30 16:41:02 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: String,
+})
+</script>
+
+<style scoped>
+/* Add any necessary styles */
+</style>
